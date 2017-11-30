e33c46f58d25cca0b4fbc477284e2e6b9e0d3b5c
Rudra A <Rudra@Sample.com>
2017-11-30 15:28:59 +0530

    First Checkin

---
+++ Test_02/ART1/src/main/java/GRP1/ART1/Test02.java
@@ -0,0 +1,12 @@
+package GRP1.ART1;
+
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class Test02 extends TestCase {
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+}
