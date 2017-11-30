24dd9b12930c620ff25e56b6ef369a81deabf15c
Rudra A <Rudra@Sample.com>
2017-11-30 15:56:19 +0530

    third commit

--- Test_02/ART1/src/main/java/GRP1/ART1/Test02.java
+++ Test_02/ART1/src/main/java/GRP1/ART1/Test02.java
@@ -7,6 +7,6 @@ import junit.framework.TestCase;
 public class Test02 extends TestCase {
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		fail("Not yet implemented_added comment");
 	}
 }
